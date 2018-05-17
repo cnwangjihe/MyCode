00ffd4e8d7b4c93dbb5bdbce0191fd62840d50e0
cnwangjihe <1321942045@qq.com>
2018-05-17 22:07:33 +0800

    2018.05.17 22:07

---
+++ ByTime/2018.05.17/#1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <string.h>
+
+using namespace std;
+
+string s="smzx",st;
+
+ofstream fout("smzx.txt");
+
+void dfs(int k,int n)
+{
+	if (k==n)
+	{
+		fout << st << endl;
+		return ;
+	}
+	for (int i=0;i<10;i++)
+	{
+		st.append(1,char(i+'0'));
+		dfs(k+1,n);
+		st.erase(k+4,1);
+	}
+		st.append(1,'_');
+		dfs(k+1,n);
+		st.erase(k+4,1);
+	return ;
+}
+
+int main()
+{
+	for (int n=0;n<=7;n++)
+	{
+		st=s;
+		dfs(0,n);
+	}
+	return 0;
+}
