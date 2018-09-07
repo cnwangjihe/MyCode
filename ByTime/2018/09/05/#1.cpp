7d13c6a9ce90b9ff28421055669467b59c5328b2
Wang Jihe <1321942045@qq.com>
2018-09-07 16:45:40 +0800

    2018.09.07 16:45

--- ByTime/2018/09/05/#1.cpp
+++ ByTime/2018/09/05/#1.cpp
@@ -4,11 +4,33 @@
 using namespace std;
 
 #define T po##pen
+#define EN get##env
+
+void expand_user(string& path)
+{
+	if (!path.empty() && path[0] == '~')
+	{
+		char const* home = EN("HOME");
+		if (home || ((home = EN("USERPROFILE"))))
+			path.replace(0, 1, home);
+		else
+		{
+			char const *hdrive = EN("HOMEDRIVE"),
+			*hpath = EN("HOMEPATH");
+			path.replace(0, 1, std::string(hdrive) + hpath);
+		}
+	}
+}
 
 int main()
 {
-	char path[10000];
-	getcwd(path,10000);
-	printf("%s\n",path);
+//	FILE * fp;
+	char buffer[80];
+	string path="~";
+	expand_user(path);
+//	fp = T("cat /etc/passwd", "r");
+//	fgets(buffer, sizeof(buffer), fp);
+	printf("%c00",path[0]);
+//	pclose(fp);
 	return 0;
 }
