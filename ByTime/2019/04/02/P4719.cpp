b410104b29e9accdd7447b96637f77c2d7ffc999
WangJihe <1321942045@qq.com>
2019-04-08 08:25:38 +0800

    20119.04.08 08:25

--- ByTime/2019/04/02/P4719.cpp
+++ ByTime/2019/04/02/P4719.cpp
@@ -21,8 +21,48 @@ private:
 public:
 	inline void flush() {fwrite(wbuf,1,wp-wbuf,stdout);wp=wbuf;}
 	~IO(){flush();}
-	inline char getchar(){return rp1==rp2&&(rp2=(rp1=rbuf)+fread(rbuf,1,100000,stdin),rp1==rp2)?-1:*rp1++;}
-	inline void putchar(char c){(wp-wbuf==100000)?(flush()):(void)0;*wp++=c;}
+	#ifndef DEBUG
+		inline char getchar(){return rp1==rp2&&(rp2=(rp1=rbuf)+fread(rbuf,1,100000,stdin),rp1==rp2)?-1:*rp1++;}
+		inline void putchar(char c){(wp-wbuf==100000)?(flush()):void();*wp++=c;}
+	#endif
+	inline void prints(const char *s)
+	{
+		int n=strlen(s);
+		for (int i=0;i<n;i++)
+			putchar(s[i]);
+		return ;
+	}
+	inline void prints(const string s)
+	{
+		int n=s.size();
+		for (int i=0;i<n;i++)
+			putchar(s[i]);
+		return ;
+	}
+	inline int scans(char *s,int (*check)(int c)=isalnum)
+	{
+		int n=0;short c=getchar();
+		while (!check(c) && (~c))
+			c=getchar();
+		if (!~c)
+			return 0;
+		do {s[n++]=c,c=getchar();}
+			while (check(c));
+		s[n]='\0';
+		return n;
+	}
+	inline int scans(string &s,int (*check)(int c)=isalnum)
+	{
+		int n=0;short c=getchar();
+		s="";
+		while (!check(c) && (~c))
+			c=getchar();
+		if (!~c)
+			return 0;
+		do {s+=c,c=getchar();n++;}
+			while (check(c));
+		return n;
+	}
 	template<typename Number> void print(Number x)
 	{
 		if (x<0)
@@ -31,22 +71,26 @@ public:
 			print(x/10);
 		putchar(x%10+48);
 	}
-	template<typename Number> void scan(Number &x)
+	template<typename Number> int scan(Number &x)
 	{
-		bool f=0;x=0;char c=getchar();
-		while (!isdigit(c))
+		bool f=0;x=0;short c=getchar();
+		while (!isdigit(c) && (~c))
 			f|=(c=='-'),c=getchar();
+		if (!~c)
+			return 0;
 		do {x=x*10+c-48,c=getchar();}
 			while (isdigit(c));
 		x=(f?x*-1:x);
-		return ;
+		return 1;
 	}
 	inline void println(){putchar('\n');}
 	#if __cplusplus >= 201103L
 		template<typename Number,typename... Numbers>
 		inline void print(Number x,Numbers... X) {print(x),putchar(' '),print(X...);}
 		template<typename Number,typename... Numbers>
-		inline void scan(Number &x,Numbers &... X) {scan(x),scan(X...);}
+		inline int scan(Number &x,Numbers &... X) {return scan(x)+scan(X...);}
+	#else
+		#warning Please use c++11 to enable all features of IO
 	#endif
 }IO;
 
@@ -251,15 +295,12 @@ int main()
 		_connect(y,x);
 	}
 	dfs1(1);
-//	cerr << "f**k1\n";
 	dfs2(1,1);
-//	cerr << "f**k2\n";
 	dfs3(1);
-//	cerr << "f**k3\n";
 	ST.Build(1,1,n);
-//	for (int i=1;i<=n;i++)
+	for (int i=1;i<=n;i++)
 //		cerr << i << ' ' << bot[i] << ' ' << w[i] << '\n';
-		// Query(1).Out();
+		Query(i).Out();
 	int ans=0;
 	for (int i=0;i<m;i++)
 	{
